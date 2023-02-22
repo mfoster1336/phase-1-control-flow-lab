bffa6fc4ca942c9d6ba5d001b034ef783c55412d
mikalfoster <mikalfoster36@gmail.com>
2023-02-21 21:18:04 -0600

    commit tree eec3124d1e81350536b1bcd64ea7896e53d83c59

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(length){
   // Write your code here!
+   if (length <= 400) {
+    return "This one is on me!";
+   }
+   if (length <= 2000) {
+    return "That will be twenty bucks."
+   }
+   if (length <= 2499) {
+    return "I will gladly take your thirty bucks."
+   }
+   if (length >= 2500) {
+    return "No can do."
+   }
 }
+console.log(scuberGreetingForFeet());
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city = NYC ? "OK, sounds good." : "No go.";
 }
+console.log(ternaryCheckCity());
 
 function switchOnCharmFromTip(){
   // Write your code here!
