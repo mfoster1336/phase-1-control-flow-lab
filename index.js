a536b122b5bb4ebac21913356fa92aadf0b322bd
mikalfoster <mikalfoster36@gmail.com>
2023-02-23 21:20:57 -0600

    Uploading again just in case

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(length){
   // Write your code here!
+   if (length <= 400) {
+    return "This one is on me!";
+   }
+   if (length <= 2000) {
+    return "That will be twenty bucks."
+   }
+   if (length <= 2499) {
+    return "I will gladly take your thirty bucks."
+   }
+   if (length >= 2500) {
+    return "No can do."
+   }
+}
+console.log(scuberGreetingForFeet());
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : "No go.";
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(switchOnCharmFromTip());
